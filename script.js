ffae3f6c5d3e432941a70604a86373fece350c73
SubhanKhalif <subhankhalif34@gmail.com>
2025-01-15 19:24:58 +0530

    added visibility tab

--- script.js
+++ script.js
@@ -13,3 +13,19 @@
   function redirectToPage(url) {
     window.location.href = url; // Redirect to the specified URL
   }
+
+          // the visibility tab
+document.addEventListener('visibilitychange',
+  function () {
+      if (document.visibilityState === "visible") {
+          document.title = "Portfolio | HARMEET KOUR";
+          $("#favicon").attr("href", "assets/logo.png");
+      }
+      else {
+          document.title = "Come Back To Website";
+          $("#favicon").attr("href", "assets/images/favhand.png");
+      }
+  });
+      // the visibility tab
+
+      
